b705326d44a62aa7e176fb5da77047284d612d4c
Lucas Hubert <hello@lucas-hubert.me>
2025-05-15 19:19:11 +0200

    save

--- src/main/java/com/lucas/tennis/service/HealthCheckService.java
+++ src/main/java/com/lucas/tennis/service/HealthCheckService.java
@@ -2,11 +2,22 @@ package com.lucas.tennis.service;
 
 import com.lucas.tennis.ApplicationStatus;
 import com.lucas.tennis.HealthCheck;
+import com.lucas.tennis.repository.HealthCheckRepository;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
 @Service
 public class HealthCheckService {
+
+    @Autowired
+    private HealthCheckRepository healthCheckRepository;
+
     public HealthCheck healthCheck() {
-        return new HealthCheck(ApplicationStatus.OK, "OK");
+        Long activeSessions = healthCheckRepository.countApplicationConnections();
+
+        if (activeSessions > 0) {
+            return new HealthCheck(ApplicationStatus.OK, "OK");
+        }
+        return new HealthCheck(ApplicationStatus.KO, "KO");
     }
 }
